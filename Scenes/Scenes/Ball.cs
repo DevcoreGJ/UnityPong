6ac3b2f1f1b22388d91016c9433dafe3548116ef
Elliot Ledson <elliotledson@gmail.com>
2023-06-19 22:54:09 +0100

    My commits are being funny...
    Here is the last couple of updates I was talking about. Hot today...

---
+++ Scenes/Scenes/Ball.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Launch is called before the first frame update
+    Private void Launch()
+    {
+        float x = Random.Range(0, 2) == = ? -1 : 1;
+        float y = Random.Range(0, 2) == = ? -1 : 1;
+        // Generates a random integer
+        // If the random value is equal to 0, the ternary operator ?
+        // Assigns the value -1 to the variable.
+        // Otherwise it assigns 1 to variable.
+
+        rb.velocity = new Vector2(speed * x, speed * y) // velocity property of rigid body component in game.
+        // creates a new Vector2 object
+        // x and y are variables that determine the direction of the velocity
+        // values of 1 or - 1
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
